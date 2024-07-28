e967696685f460f815663b194022be2c6338fe42
breyes97 <betty970816@gmail.com>
2024-07-28 11:26:17 -0400

    Update

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CasosUsoLynn.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
